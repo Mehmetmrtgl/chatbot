e89ba2743fc87115d4ea8487c58be7d0a6a677e6
Mehmetmrtgl <mehmetmrt1999@gmail.com>
2025-12-12 17:27:09 +0300

    changed port id for mac, requirements edited

--- app/app.py
+++ app/app.py
@@ -33,4 +33,4 @@ def create_app():
 
 if __name__ == "__main__":
     flask_app = create_app()
-    flask_app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5000)
+    flask_app.run(debug=True, use_reloader=False, host="0.0.0.0", port=5001)
